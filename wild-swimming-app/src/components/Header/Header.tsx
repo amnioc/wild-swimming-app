477f18d12b4af8eb9172c117344461254196b49f
Your Name <youremail@domain.com>
2023-05-03 10:31:39 +0100

    removed user.email

--- wild-swimming-app/src/components/Header/Header.tsx
+++ wild-swimming-app/src/components/Header/Header.tsx
@@ -43,7 +43,6 @@ const Header = () => {
           <p>You are logged in</p>
           <img src={user.picture} alt={user.name} />
           <h2>{user.name}</h2>
-          <p>{user.email}</p>
         </div>
       )}
       <LogoutButton />
