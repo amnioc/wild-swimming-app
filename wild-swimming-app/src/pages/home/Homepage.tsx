94a9182ae31c1a142b617deb5e4a94d67fef02a4
Your Name <youremail@domain.com>
2023-05-04 22:25:28 +0100

    added homepage picture

--- wild-swimming-app/src/pages/home/Homepage.tsx
+++ wild-swimming-app/src/pages/home/Homepage.tsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../feature/footer/Footer";
+import homepage from "./Homepage.png";
+
 const Homepage = () => {
   return (
     <section>
       <Header />
-      <p>Homepage</p>
+      <img src={homepage} width="2000px" height="1000px" alt="Homepage" />
       <Footer />
     </section>
   );
