62e9c02570d013b56bfdeb05ed7f7d888b32929e
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-04 14:12:43 +0100

    refactor router

--- wild-swimming-app/src/App.tsx
+++ wild-swimming-app/src/App.tsx
@@ -1,22 +1,22 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import RootLayout from "./components/RootLayout/RootLayout";
 import Homepage from "./pages/home/Homepage";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Homepage />} />
-    </Route>
-  )
-);
+import LocationDetails from "./pages/location-details/Location-details";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <section>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Homepage />} />
+        </Route>
+
+        <Route path="/location/:id" element={<LocationDetails />} />
+      </Routes>
+    </section>
+  );
 }
 
 export default App;
