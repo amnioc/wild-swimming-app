cfe6d08412dc37018620a25e00605f7f7e000744
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-05 17:03:32 +0100

    add map to homepage

--- wild-swimming-app/src/pages/home/Homepage.tsx
+++ wild-swimming-app/src/pages/home/Homepage.tsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/Header";
+import Map from "../../feature/map/Map";
 
 const Homepage = () => {
   return (
     <section>
       <Header />
       <p>Homepage</p>
+      <Map />
     </section>
   );
 };
