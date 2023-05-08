fbe6b5099c56204645a9793b3ec9e67976778db6
amnioc <nicdhillon19@gmail.com>
2023-05-08 16:01:21 +0100

    comment card and section awaiting BE

--- wild-swimming-app/src/feature/comments/Comments.tsx
+++ wild-swimming-app/src/feature/comments/Comments.tsx
@@ -1,27 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CommentCard from "../../components/comment-card/Comment-card";
 import styles from "./comments.module.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useSearchParams } from "react-router-dom";
 
-const Comments = () => {
-  const [comments, setComments] = useState("");
+const Comments = (location_id) => {
+  const [locationComments, setLocationComments] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
   const { user } = useAuth0();
+  const { location_id } = useSearchParams; // is this correct?
 
-  console.log(user);
-  //get comments by location ID
+  useEffect(() => {
+    setIsLoading(true);
+    getCommentsByLocation(location_id).then((comments) => {
+      setIsLoading(false);
+      setLocationComments(comments);
+    });
+  }, [location_id]);
+
+  if (isLoading) {
+    {
+      err ? <h4>{err}</h4> : null;
+    }
+    return <h4 className={styles.loadingMessage}>Comments Loading...</h4>;
+  }
   //map comments
 
   return (
-    <ul className={styles.commentsList}>
-      {comments.map((comment) => {
-        return (
-          <section>
-            {err ? <h4>{err}</h4> : null}
-            <CommentCard user={user} comment={comment} />
-          </section>
-        );
-      })}
-    </ul>
+    <section className={styles.commentsSection}>
+      {/* <New Comment Form> */}
+      <ul className={styles.commentsList}>
+        {currentComments.map((comment) => {
+          return (
+            <section>
+              <CommentCard user={user} comment={comment} />
+            </section>
+          );
+        })}
+      </ul>
+    </section>
   );
 };
 
