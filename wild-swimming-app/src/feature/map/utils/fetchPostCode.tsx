994fc8eed4f426635a11460771bda3dd0a67e821
dreamingOfSoy <dreamingofsoy@gmail.com>
2023-05-09 13:38:23 +0100

    fixed postcode bug

--- wild-swimming-app/src/feature/map/utils/fetchPostCode.tsx
+++ wild-swimming-app/src/feature/map/utils/fetchPostCode.tsx
@@ -1,21 +1,20 @@
 import axios from "axios";
 import { error } from "console";
 
-export const checkValidPostocde =(postcode:any) => {
-
-  return axios.get(`https://postcodes.io/postcodes/
-  ${postcode}/validate`).then((response)=>{
-  
-    return response.data.result 
-  })
-  }
-
-
+export const checkValidPostocde = (postcode: any) => {
+  return axios
+    .get(
+      `https://postcodes.io/postcodes/
+  ${postcode}/validate`
+    )
+    .then((response) => {
+      console.log(response);
+      return response.data.result;
+    });
+};
 
 export const fetchPostCode = (postcode: any) => {
- 
-  return  axios.get(`https://postcodes.io/postcodes/${postcode}`).then((res) => {
-   
+  return axios.get(`https://postcodes.io/postcodes/${postcode}`).then((res) => {
     let num1 = res.data.result.longitude;
     let num2 = res.data.result.latitude;
     const postcodeLocation = {
