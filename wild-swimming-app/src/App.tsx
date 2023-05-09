2f7e73e3e56d1416f9f0edafae5effa450dd2bae
Nico <120725313+amnioc@users.noreply.github.com>
2023-05-09 12:08:48 +0100

    Update App.tsx

--- wild-swimming-app/src/App.tsx
+++ wild-swimming-app/src/App.tsx
@@ -1,28 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import RootLayout from "./components/RootLayout/RootLayout";
 import Homepage from "./pages/home/Homepage";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-const { user, isAuthenticated, isLoading } = useAuth0();
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Homepage />} />
-    </Route>
-  )
-);
-
 import LocationDetails from "./pages/location-details/Location-details";
 import TsCs from "./pages/ts-cs/Ts-cs";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <section>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Homepage />} />
+        </Route>
+
+        <Route path="terms" element={<TsCs />} />
+        <Route path="location/:id" element={<LocationDetails />} />
+      </Routes>
+    </section>
+  );
 }
 
 export default App;
