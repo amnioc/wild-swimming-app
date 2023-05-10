be7dc001a5a5345ba8b927779960ff2cb2736b81
Your Name <youremail@domain.com>
2023-05-10 15:14:58 +0100

    some contents in about page

--- wild-swimming-app/src/App.tsx
+++ wild-swimming-app/src/App.tsx
@@ -5,7 +5,7 @@ import RootLayout from "./components/RootLayout/RootLayout";
 import Homepage from "./pages/home/Homepage";
 import LocationDetails from "./pages/location-details/Location-details";
 import TsCs from "./pages/ts-cs/Ts-cs";
-
+import About from "./pages/about/About";
 import Footer from "./feature/footer/Footer";
 import Header from "./components/Header/Header";
 
@@ -19,6 +19,7 @@ function App() {
           <Route index element={<Homepage />} />
 
           <Route path="terms" element={<TsCs />} />
+          <Route path="about" element={<About />} />
           <Route path="location/:id" element={<LocationDetails />} />
         </Route>
       </Routes>
